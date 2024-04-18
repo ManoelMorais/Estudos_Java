96c11c22fa084d6861bd8f88c94d289a54c2823e
Manoel <999142818m@gmail.com>
2024-04-18 16:22:28 -0300

    3 Criação do desenho do tabuleiro

--- src/JogoXadrez/Principal.java
+++ src/JogoXadrez/Principal.java
@@ -1,7 +1,10 @@
 package JogoXadrez;
 
+import JogoXadrez.Chess.ChessMatch;
+
 public class Principal {
     public static void main(String[] args){
-
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
