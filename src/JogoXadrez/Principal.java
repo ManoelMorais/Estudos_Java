c57c1792c56cf56eb966090e61fa084ff6fcba0e
Manoel <999142818m@gmail.com>
2024-04-18 15:46:37 -0300

    2 Criação do Board e Piece

--- src/JogoXadrez/Principal.java
+++ src/JogoXadrez/Principal.java
@@ -1,11 +1,7 @@
 package JogoXadrez;
 
-import JogoXadrez.boardgame.Position;
-
 public class Principal {
     public static void main(String[] args){
-        Position position = new Position(3, 5);
 
-        System.out.println(position);
     }
 }
