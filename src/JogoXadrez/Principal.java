4a40ae02f8d44bafe2cb9455385915b7fbdf9518
Manoel <999142818m@gmail.com>
2024-04-18 14:50:10 -0300

    1 criação do projeto e Position

---
+++ src/JogoXadrez/Principal.java
@@ -0,0 +1,11 @@
+package JogoXadrez;
+
+import JogoXadrez.boardgame.Position;
+
+public class Principal {
+    public static void main(String[] args){
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
+    }
+}
