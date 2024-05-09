c38a72b46e85773aa3f35de83c9819afb23b4ebb
Manoel <999142818m@gmail.com>
2024-05-09 17:50:12 -0300

    JDBC, conectando ao mysql, tudo ok

---
+++ src/JDBC/DB.java
@@ -0,0 +1,46 @@
+package JDBC;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() throws IOException {
+        if ( conn == null ) {
+            try {
+                Properties prop = loadProperties();
+                String url = prop.getProperty("dburl");
+                conn = DriverManager.getConnection(url, prop);
+            } catch (SQLException e) {
+                throw new MyJDBC(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new MyJDBC(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties() throws IOException {
+        try(FileInputStream fs = new FileInputStream("db.properies")) {
+            Properties prop = new Properties();
+            prop.load(fs);
+            return prop;
+        } catch (IOException e) {
+            throw new MyJDBC(e.getMessage());
+        }
+    }
+}
---
+++ src/JDBC/MyJDBC.java
@@ -0,0 +1,5 @@
+package JDBC;
+
+public class MyJDBC extends RuntimeException {
+    public MyJDBC(String msg) {super(msg);}
+}
