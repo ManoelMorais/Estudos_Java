1df291753b1aac81fe8270e993315ff53d422d2e
Manoel <999142818m@gmail.com>
2024-05-14 15:11:22 -0300

    criação do seller e testes

--- src/JDBC/PROJETO/Program.java
+++ src/JDBC/PROJETO/Program.java
@@ -1,9 +1,13 @@
 package JDBC.PROJETO;
 
+import java.util.Date;
+
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
+        Seller seller = new Seller(21, "boby", "boby@gmail.com", new Date(), 3000.0, dp);
 
-        System.out.println(dp);
+        //System.out.println(dp);
+        System.out.println(seller);
     }
 }
