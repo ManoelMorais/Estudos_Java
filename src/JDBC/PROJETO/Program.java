f890459a78a0236576e72274810ae6ecda4e2fde
Manoel <999142818m@gmail.com>
2024-05-14 15:02:36 -0300

    criação dodepartment e testando para saber se funciona

---
+++ src/JDBC/PROJETO/Program.java
@@ -0,0 +1,9 @@
+package JDBC.PROJETO;
+
+public class Program {
+    public static void main(String[] args) {
+        Department dp = new Department(1, "Books");
+
+        System.out.println(dp);
+    }
+}
