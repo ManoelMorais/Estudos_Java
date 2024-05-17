70be714d6bcaca0edd0cd2b9aca6970bdfcc1007
Manoel <999142818m@gmail.com>
2024-05-17 10:12:46 -0300

    Construção do Update

--- src/JDBC/PROJETO/SellerDaoJDBC.java
+++ src/JDBC/PROJETO/SellerDaoJDBC.java
@@ -57,7 +57,28 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller seller) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ? , BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE id = ?"
+            );
 
+            st.setString(1, seller.getNome());
+            st.setString( 2, seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble( 4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbExecption(e.getMessage());
+        } finally {
+            DB.closeStatment(st);
+        }
     }
 
     @Override
