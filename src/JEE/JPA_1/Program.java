855b46bd8b8926a4a954215db772535d1f09696a
Manoel <999142818m@gmail.com>
2024-05-21 09:09:01 -0300

    JPA_1

---
+++ src/JEE/JPA_1/Program.java
@@ -0,0 +1,9 @@
+package JEE.JPA_1;
+
+public class Program {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "João", "joao@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Maria", "maria@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Flor", "flor@gmail.com");
+    }
+}
---
+++ src/JEE/aaaa.java
@@ -0,0 +1,4 @@
+package JEE;
+
+public class aaaa {
+}
